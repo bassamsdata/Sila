50b920d22358fd4a94c0ae974006cbbc4c42d3e3
Bassam Data <105807570+bassamsdata@users.noreply.github.com>
2023-11-03 22:21:00 -0400

    add options

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -12,3 +12,57 @@ opt.number = true
 -- Enable mouse mode.
 vim.o.mouse = 'a'
 
+-- Save undo history.
+vim.o.undofile = true
+opt.undolevels = 10000
+
+-- Confirm to save changes before exiting modified buffer
+opt.confirm = true 
+
+-- Use ripgrep for grepping.
+vim.o.grepprg = 'rg --vimgrep'
+vim.o.grepformat = '%f:%l:%c:%m'
+
+-- tabs & indentation
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+--line wrapping
+opt.wrap = false
+
+-- search settings
+opt.ignorecase = true
+opt.smartcase = true
+
+-- Cursor settings
+opt.cursorline = true
+opt.guicursor = "a:hor25,v:block,i:ver25"
+
+-- Appearance
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+-- backsapace
+opt.backspace = "indent,eol,start"
+
+-- clipoboard
+opt.clipboard:append("unnamedplus")
+
+--spli windows
+opt.splitright = true
+opt.splitbelow = true
+
+-- TESTING: consider dash - as one word
+opt.iskeyword:append("-")
+
+-- UI characters.
+opt.fillchars = {
+    eob = ' ',
+  }
+
+if vim.fn.has("nvim-0.10") == 1 then
+  opt.smoothscroll = true
+end
