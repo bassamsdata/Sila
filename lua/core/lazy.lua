240209260efde7a5671a2101151a9c1f7be9315a
Bassam Data <105807570+bassamsdata@users.noreply.github.com>
2023-11-03 22:27:09 -0400

    customizing lazy.nvim

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -11,3 +11,38 @@ if not vim.loop.fs_stat(lazypath) then
 	})
 end
 vim.opt.rtp:prepend(lazypath)
+
+-- use a protected call so we don't error out on first use
+local lazy = Prequire("lazy")
+if not lazy then
+	return
+end
+
+--- @type LazySpec
+local plugins = "plugins"
+local lsp = "plugins.lsp"
+
+-- load plugins from specifications (The leader key must be set before this)
+lazy.setup({ { import = plugins }, { import = lsp } }, {
+	ui = { border = "rounded" },
+	install = {
+		-- Do not automatically install on startup.
+		missing = false,
+	},
+	-- I like to play with my configs alot so less clutter please.
+	change_detection = { notify = false },
+	performance = {
+		rtp = {
+			-- Stuff I don't use.
+			disabled_plugins = {
+				"gzip",
+				"netrwPlugin",
+				"rplugin",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+			},
+		},
+	},
+})
