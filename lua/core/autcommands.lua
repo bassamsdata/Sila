2d9028229e19160d64de1d1fb46438afd3e26d6c
Bassam Data <105807570+bassamsdata@users.noreply.github.com>
2023-11-07 22:25:06 -0500

    autocmd to close ft with q

--- lua/core/autcommands.lua
+++ lua/core/autcommands.lua
@@ -10,3 +10,23 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank()
 	end,
 })
+
+-- close some filetypes with <q>
+vim.api.nvim_create_autocmd("FileType", {
+	group = augroup("close_with_q"),
+	pattern = {
+		"help",
+		"lspinfo",
+		"man",
+		"notify",
+		"qf",
+		"query",
+		"spectre_panel",
+		"startuptime",
+		"checkhealth",
+	},
+	callback = function(event)
+		vim.bo[event.buf].buflisted = false
+		vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+	end,
+})
