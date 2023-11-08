44929fdd94d2608f7344be2508e4ba8dcec8f667
Bassam Data <105807570+bassamsdata@users.noreply.github.com>
2023-11-07 22:19:34 -0500

    reorder the loading

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,7 +1,7 @@
 -- set leader key to space
 vim.g.mapleader = " "
 -- Set <\> as the local leader key - it gives me a whole new set of letters.
-vim.g.maplocalleader = '\\' -- we need to escabe \ with another \
+vim.g.maplocalleader = "\\" -- we need to escabe \ with another \
 
 local opt = vim.opt -- for concisenes
 
@@ -10,24 +10,29 @@ opt.relativenumber = true
 opt.number = true
 
 -- Enable mouse mode.
-vim.o.mouse = 'a'
-
--- Save undo history.
-vim.o.undofile = true
-opt.undolevels = 10000
-
--- Confirm to save changes before exiting modified buffer
-opt.confirm = true 
-
--- Use ripgrep for grepping.
-vim.o.grepprg = 'rg --vimgrep'
-vim.o.grepformat = '%f:%l:%c:%m'
+opt.mouse = "a"
 
 -- tabs & indentation
 opt.tabstop = 2
 opt.shiftwidth = 2
 opt.expandtab = true
-opt.autoindent = true
+-- opt.autoindent = true
+opt.shiftround = true -- Round indent
+
+-- Save undo history.
+opt.undofile = true
+opt.undolevels = 10000
+
+opt.statuscolumn = [[%!v:lua.require'core.util'.statuscolumn()]]
+
+-- Confirm to save changes before exiting modified buffer
+opt.confirm = true
+
+-- Use ripgrep for grepping.
+opt.grepprg = "rg --vimgrep"
+opt.grepformat = "%f:%l:%c:%m"
+
+opt.pumheight = 10 -- Maximum number of entries in a popup
 
 --line wrapping
 opt.wrap = false
@@ -39,6 +44,7 @@ opt.smartcase = true
 -- Cursor settings
 opt.cursorline = true
 opt.guicursor = "a:hor25,v:block,i:ver25"
+opt.termguicolors = true -- True color support
 
 -- Appearance
 opt.termguicolors = true
@@ -46,7 +52,7 @@ opt.background = "dark"
 opt.signcolumn = "yes"
 
 -- backsapace
-opt.backspace = "indent,eol,start"
+-- opt.backspace = "indent,eol,start"
 
 -- clipoboard
 opt.clipboard:append("unnamedplus")
@@ -56,13 +62,22 @@ opt.splitright = true
 opt.splitbelow = true
 
 -- TESTING: consider dash - as one word
-opt.iskeyword:append("-")
+opt.iskeyword:append("_")
+-- opt.formatoptions = "jcroqlnt" -- tcqj
 
+-- this drove me crzy - it controll how vertical movement behave when tab is used
+opt.list = true
 -- UI characters.
 opt.fillchars = {
-    eob = ' ',
-  }
+	foldopen = "",
+	foldclose = "",
+	-- fold = "⸱",
+	fold = " ",
+	foldsep = " ",
+	diff = "╱",
+	eob = " ",
+}
 
 if vim.fn.has("nvim-0.10") == 1 then
-  opt.smoothscroll = true
+	opt.smoothscroll = true
 end
