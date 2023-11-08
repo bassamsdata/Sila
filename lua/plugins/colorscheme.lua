e5ca1d63a141c2a0cca0f3b8e99a784a4b831bc8
Bassam Data <105807570+bassamsdata@users.noreply.github.com>
2023-11-07 22:21:39 -0500

    adding comments and conditional

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,2 +1,19 @@
+return {
+	{
+		"Mofiqul/dracula.nvim",
+		priority = 1000, -- make sure to load this before all the other start plugins
+		opts = {
+			colors = {
+				bg = "#07080f",
+				fg = "#C0B9DD",
+			},
+			transparent_bg = true,
 			italic_comment = true,
+			overrides = {
+				CursorLine = { bg = "#101c24" },
+				NormalFloat = { bg = nil }, --#transparency
 				["@conditional"] = { italic = true },
+			},
+		},
+	},
+}
