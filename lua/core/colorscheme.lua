d639f8e4b27ad92c3574e73b6a672dac64f55684
Bassam Data <105807570+bassamsdata@users.noreply.github.com>
2023-11-07 22:22:08 -0500

    adding protected call for colorscheme

---
+++ lua/core/colorscheme.lua
@@ -0,0 +1,12 @@
+-- ensure the colorscheme is loaded so we do not see erorrs
+local colorscheme = "dracula"
+---@diagnostic disable-next-line: param-type-mismatch
+local ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+-- TODO: remove this from the options file
+vim.o.background = "dark" -- or "light" for light mode
+if not ok then
+	-- Set a default colorscheme.
+	vim.cmd.colorscheme("habamax")
+	vim.notify("colorscheme " .. colorscheme .. " not found!")
+	return
+end
