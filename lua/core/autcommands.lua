9a9c03993914279e05563ef1c02e50937e33bc83
Bassam Data <105807570+bassamsdata@users.noreply.github.com>
2023-11-07 22:25:26 -0500

    autcmd creating autodir if not exist

--- lua/core/autcommands.lua
+++ lua/core/autcommands.lua
@@ -30,3 +30,15 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
 	end,
 })
+
+-- Auto create dir when saving a file, in case some intermediate directory does not exist
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+	group = augroup("auto_create_dir"),
+	callback = function(event)
+		if event.match:match("^%w%w+://") then
+			return
+		end
+		local file = vim.loop.fs_realpath(event.match) or event.match
+		vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+	end,
+})
