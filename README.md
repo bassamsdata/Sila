84387350b807cbd28c2de2dadddf5bb9d2b15c9c
Bassam Data <105807570+bassamsdata@users.noreply.github.com>
2023-11-13 00:28:08 -0500

    Adding readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# Sila Neovim Config
+--------------------------
+this config is for the beloved neovim.
+
+This is still work in progress and the readme currently just to track the TODO list.
+
+
+# TODO:
+- Statusline
+- Terminal
+- Folding - statuscol.nvim plugin
+- Colorscheme
+  - Highlights
+  - Cursor highlight change
--- lua/plugins/minipick.lua
+++ lua/plugins/minipick.lua
@@ -3,21 +3,40 @@ return {
 		"echasnovski/mini.pick",
 		dependencies = { "echasnovski/mini.extra", opts = {} },
 		cmd = "Pick",
-		opts = {
-			window = {
-				config = function()
-					local height = math.floor(0.55 * vim.o.lines)
-					local width = math.floor(0.55 * vim.o.columns)
+		config = function()
+			local MiniPick = require("mini.pick")
 
-					return {
-						anchor = "NW",
-						height = height,
-						width = width,
-						row = math.floor(0.5 * (vim.o.lines - height)),
-						col = math.floor(0.5 * (vim.o.columns - width)),
-					}
-				end,
-			},
-		},
+			-- Setup 'mini.pick'
+			MiniPick.setup({})
+
+			-- File picker configuration
+			local cursor_win_config = {
+				relative = "cursor",
+				anchor = "NW",
+				row = 0,
+				col = 0,
+				width = 40,
+				height = 20,
+			}
+
+			-- Modify the 'files' picker directly
+			MiniPick.registry.files = function()
+				return MiniPick.builtin.files({}, { window = { config = cursor_win_config } })
+			end
+			-- Help picker configuration
+			local right_win_config = {
+				relative = "editor",
+				anchor = "SE",
+				row = vim.o.lines,
+				col = vim.o.columns,
+				height = 20,
+				width = 40,
+			}
+
+			-- Modify the 'help' picker directly
+			MiniPick.registry.buffers = function()
+				return MiniPick.builtin.buffers({}, { window = { config = right_win_config } })
+			end
+		end,
 	},
 }
