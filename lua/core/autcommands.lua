5d3c488fe4a2991224f0eef9736aca8c68eba7c0
Bassam Data <105807570+bassamsdata@users.noreply.github.com>
2023-11-17 21:17:01 -0500

    pcall for mini.map autocmmand

--- lua/core/autcommands.lua
+++ lua/core/autcommands.lua
@@ -11,6 +11,83 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
+-- go to last loc when opening a buffer
+vim.api.nvim_create_autocmd("BufReadPost", {
+	group = augroup("last_loc"),
+	callback = function(event)
+		local exclude = { "gitcommit" }
+		local buf = event.buf
+		if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].lazyvim_last_loc then
+			return
+		end
+		vim.b[buf].lazyvim_last_loc = true
+		local mark = vim.api.nvim_buf_get_mark(buf, '"')
+		local lcount = vim.api.nvim_buf_line_count(buf)
+		if mark[1] > 0 and mark[1] <= lcount then
+			pcall(vim.api.nvim_win_set_cursor, 0, mark)
+		end
+	end,
+})
+
+-- Change the current line number depend on neovim mode
+-- similar to modicator.nvim plugin
+-- vim.api.nvim_create_autocmd({ "ModeChanged" }, {
+--   group = augroup("modechange"),
+-- 	callback = function()
+-- 		-- make it work on statuscolumn custom numbering (with ranges over visual selection)
+-- 		local hl = vim.api.nvim_get_hl(0, { name = U.get_mode_hl() or "Normal" })
+-- 		local curline_hl = vim.api.nvim_get_hl(0, { name = "CursorLine" })
+-- 		vim.api.nvim_set_hl(0, "CursorLineNr", { fg = hl.fg, bg = curline_hl.bg })
+-- 	end,
+-- })
+
+-- TODO: move tihs function to helpers
+-- =====** HELP_GREP function **==========================================
+
+-- -- Load the 'mini.pick' module
+-- local MiniPick = require("mini.pick")
+--
+-- -- Define a function to search the help documentation and open the results in the quickfix list picker
+-- local function help_grep(args)
+-- 	-- Construct the pattern from the arguments
+-- 	local pattern = table.concat(args.fargs, " ")
+--
+-- 	-- Search the help documentation
+-- 	vim.api.nvim_command("helpgrep " .. pattern)
+--
+-- 	-- Open the results in the quickfix list picker
+-- 	MiniPick.start({ source = MiniPick.registry.quickfix() })
+-- end
+--
+-- -- Define a new command
+-- vim.api.nvim_create_user_command("HelpGrep", help_grep, { nargs = "*", complete = "shellcmd" })
+--========================================================================
+--
+--
+-- =========================================================================
+-- Mini Modules
+-- =========================================================================
+-- open on buffer enter
+local ok, _ = pcall(require, "mini.map")
+if not ok then
+	return
+else
+	vim.api.nvim_create_autocmd("BufReadPost", {
+		callback = function()
+			if vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":t") ~= "Starter" then
+				require("mini.map").open()
+			end
+		end,
+	})
+	-- refresh on window resize
+	vim.api.nvim_create_autocmd("VimResized", {
+		callback = function()
+			require("mini.map").refresh()
+		end,
+	})
+end
+--
+--
 -- close some filetypes with <q>
 vim.api.nvim_create_autocmd("FileType", {
 	group = augroup("close_with_q"),
