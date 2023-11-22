a8cf3c64f5487511fd13da2284be8c5b1e119087
Bassam Data <105807570+bassamsdata@users.noreply.github.com>
2023-11-22 18:43:08 -0500

    modify surround mappings

--- lua/plugins/minipairs.lua
+++ lua/plugins/minipairs.lua
@@ -21,17 +21,24 @@ return {
 	},
 	{
 		"echasnovski/mini.surround",
-		keys = { "gs" },
-		opts = {
-			mappings = {
-				add = "gsa", -- Add surrounding in Normal and Visual modes
-				delete = "gsd", -- Delete surrounding
-				find = "gsf", -- Find surrounding (to the right)
-				find_left = "gsF", -- Find surrounding (to the left)
-				highlight = "gsh", -- Highlight surrounding
-				replace = "gsr", -- Replace surrounding
-				update_n_lines = "gsn", -- Update `n_lines`
-			},
-		},
+		keys = { "gs", { "gs", mode = "v" } },
+		config = function()
+			local MiniSurround = require("mini.surround")
+			MiniSurround.setup({
+				mappings = {
+					add = "gsa", -- Add surrounding in Normal and Visual modes
+					delete = "gsd", -- Delete surrounding
+					find = "gsf", -- Find surrounding (to the right)
+					find_left = "gsF", -- Find surrounding (to the left)
+					highlight = "gsh", -- Highlight surrounding
+					replace = "gsr", -- Replace surrounding
+					update_n_lines = "gsn", -- Update `n_lines`
+				},
+			})
+			-- I don't why but clue picked up long desc, so this is just for aesthtics
+			if pcall(require, "mini.surround") then
+				require("mini.clue").set_mapping_desc("n", "gsn", "Update n# lines")
+			end
+		end,
 	},
 }
