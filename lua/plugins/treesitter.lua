6709f7b823c33d1b35f5aa2394b1f1f63ddc9823
Bassam Data <105807570+bassamsdata@users.noreply.github.com>
2023-11-22 18:37:37 -0500

    modifying treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -8,62 +8,67 @@ return {
 		{ "<C-cr>", desc = "Increment selection" },
 		{ "<bs>", desc = "Decrement selection", mode = "x" },
 	},
-	opts = {
-		highlight = { enable = true },
-		indent = { enable = true },
-		ensure_installed = {
-			"bash",
-			"diff",
-			"html",
-			"json",
-			"lua",
-			"luadoc",
-			"luap",
-			"markdown",
-			"markdown_inline",
-			"python",
-			"query",
-			"regex",
-			"toml",
-			"vim",
-			"vimdoc",
-			"yaml",
-		},
-		incremental_selection = {
-			enable = true,
-			keymaps = {
-				init_selection = "<C-cr>",
-				node_incremental = "<C-cr>",
-				scope_incremental = false,
-				node_decremental = "<bs>",
-			},
-		},
-		-- textobjects = {
-		-- 	move = {
-		-- 		enable = true,
-		-- 		goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
-		-- 		goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
-		-- 		goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
-		-- 		goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
-		-- 	},
-		-- },
-	},
-	-- ensure that each language in the ensure_installed table is unique.
-	---@diagnostic disable-next-line: undefined-doc-name
-	---@param opts TSConfig
-	config = function(_, opts)
-		if type(opts.ensure_installed) == "table" then
-			---@type table<string, boolean>
-			local added = {}
-			---@diagnostic disable-next-line: inject-field
-			opts.ensure_installed = vim.tbl_filter(function(lang)
-				if added[lang] then
-					return false
-				end
-				added[lang] = true
-				return true
-			end, opts.ensure_installed)
-		end
-		require("nvim-treesitter.configs").setup(opts)
+	config = function()
+		vim.defer_fn(function()
+			require("nvim-treesitter.configs").setup({
+				highlight = { enable = true },
+				indent = { enable = true },
+				ensure_installed = {
+					"bash",
+					"diff",
+					"html",
+					"json",
+					"lua",
+					"luadoc",
+					"luap",
+					"markdown",
+					"markdown_inline",
+					"python",
+					"query",
+					"regex",
+					"toml",
+					"vim",
+					"vimdoc",
+					"yaml",
+					"r",
+				},
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<C-cr>",
+						node_incremental = "<C-cr>",
+						scope_incremental = false,
+						node_decremental = "<bs>",
+					},
+				},
+				-- textobjects = {
+				-- 	move = {
+				-- 		enable = true,
+				-- 		goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
+				-- 		goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
+				-- 		goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
+				-- 		goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
+				-- 	},
+				-- },
+			})
+		end, 500)
 	end,
+	-- -- ensure that each language in the ensure_installed table is unique.
+	-- ---@diagnostic disable-next-line: undefined-doc-name
+	-- ---@param opts TSConfig
+	-- config = function(_, opts)
+	--   if type(opts.ensure_installed) == "table" then
+	--     ---@type table<string, boolean>
+	--     local added = {}
+	--     ---@diagnostic disable-next-line: inject-field
+	--     opts.ensure_installed = vim.tbl_filter(function(lang)
+	--       if added[lang] then
+	--         return false
+	--       end
+	--       added[lang] = true
+	--       return true
+	--     end, opts.ensure_installed)
+	--   end
+	--   require("nvim-treesitter.configs").setup(opts)
+	-- end,
 }
