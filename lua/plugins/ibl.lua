2bbee300a423d7f887ee4debbc1503d226b013af
Bassam Data <105807570+bassamsdata@users.noreply.github.com>
2023-11-22 16:02:54 -0500

    adding ibl

--- lua/plugins/ibl.lua
+++ lua/plugins/ibl.lua
@@ -1,30 +1,40 @@
 -- whitespace and indentation guides.
 return {
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		main = "ibl",
-		event = { "BufReadPost", "BufWritePost", "BufNewFile" },
-		-- for setting shiftwidth and tabstop automatically.
-		-- dependencies = "tpope/vim-sleuth",
-		opts = {
-			indent = {
-				char = "│",
-				tab_char = "│",
-			},
-			scope = {
-				show_start = false,
-				show_end = true,
-				-- enabled = true,
-			},
-			exclude = {
-				filetypes = {
-					"help",
-					"starter",
-					"lazy",
-					"mason",
-					"notify",
-				},
-			},
-		},
-	},
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+    -- for setting shiftwidth and tabstop automatically.
+    -- dependencies = "tpope/vim-sleuth",
+    config = function ()
+      vim.defer_fn(function() -- delay ibl so it doesn't affect nvim "filename" startup
+        require("ibl").setup({
+          indent = {
+          char = "│",
+          tab_char = "│",
+        },
+          scope = {
+            -- enabled = false, -- I could use mini indentation module
+            show_start = false,
+            show_end = false,
+            highlight = "CursorLineNr",
+            include = { -- this is to highlight some additional node_type :h ibl.scope
+            -- if something wierd happens I could use: 
+            -- lua = { "return_statement", "table_constructor" }
+              node_type = {lua = {"*"}},
+            }
+          },
+          exclude = {
+            filetypes = {
+              "help",
+              "starter",
+              "lazy",
+              "mason",
+              "notify",
+            },
+          },
+        })
+      end ,20) -- in milliseconds
+    end
+  },
 }
