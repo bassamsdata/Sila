94397e20af42c3df6234e0302277080b4a483c26
Bassam Data <105807570+bassamsdata@users.noreply.github.com>
2023-12-10 21:54:48 -0500

    customizing mini.pick

--- lua/plugins/minipick.lua
+++ lua/plugins/minipick.lua
@@ -7,39 +7,89 @@ return {
 			local MiniPick = require("mini.pick")
 			local MiniExtra = require("mini.extra")
 
-			-- Setup 'mini.pick'
-			MiniPick.setup({})
+			-- copy the item under the cursor
+			local copy_to_register = {
+				char = "<C-y>",
+				func = function()
+					local current_line = vim.fn.getline(".")
+					vim.fn.setreg('"', current_line)
+				end,
+			}
+			-- scrll by margin of 4 lines to not diorient
+			local scroll_up = {
+				char = "<C-k>",
+				func = function()
+					-- this is special character in insert mode press C-v then y
+					-- while holding Ctrl to invoke
+					vim.cmd("normal! 4")
+				end,
+			}
+			local scroll_down = {
+				char = "<C-j>",
+				func = function()
+					vim.cmd("norm! 4")
+				end,
+			}
+
+			-- Setup 'mini.pick' with default window in the ceneter
+			MiniPick.setup({
+				use_cache = true,
+				window = {
+					config = function()
+						local height = math.floor(0.618 * vim.o.lines)
+						local width = math.floor(0.618 * vim.o.columns)
+						return {
+							anchor = "NW",
+							height = height,
+							width = width,
+							row = math.floor(0.5 * (vim.o.lines - height)),
+							col = math.floor(0.5 * (vim.o.columns - width)),
+						}
+					end,
+				},
+				mappings = {
+					copy_to_register = copy_to_register,
+					scroll_up_a_bit = scroll_up,
+					scroll_down_a_bit = scroll_down,
+				},
+			})
+			vim.ui.select = MiniPick.ui_select
+			-- == Styles ==
+			-- 1 - File picker configuration that follows cursor
+			local cursor_win_config = {
+				relative = "cursor",
+				anchor = "NW",
+				row = 0,
+				col = 0,
+				width = 50,
+				height = 16,
+			}
+			-- 2 - Right side picker configuration
+			local right_win_config = {
+				relative = "editor",
+				anchor = "SE",
+				row = vim.o.lines,
+				col = vim.o.columns,
+				height = 16,
+				width = 50,
+			}
 
 			-- Define a new picker for the quickfix list
 			MiniPick.registry.quickfix = function()
 				return MiniExtra.pickers.list({ scope = "quickfix" }, {})
 			end
 
-			-- File picker configuration
-			local cursor_win_config = {
-				relative = "cursor",
-				anchor = "NW",
-				row = 0,
-				col = 0,
-				width = 40,
-				height = 20,
-			}
-
 			-- Modify the 'files' picker directly
 			MiniPick.registry.files = function()
 				return MiniPick.builtin.files({}, { window = { config = cursor_win_config } })
 			end
-			-- Help picker configuration
-			local right_win_config = {
-				relative = "editor",
-				anchor = "SE",
-				row = vim.o.lines,
-				col = vim.o.columns,
-				height = 20,
-				width = 40,
-			}
 
-			-- Modify the 'help' picker directly
+			-- Modify the 'old_files' picker directly
+			MiniPick.registry.oldfiles = function()
+				return MiniExtra.pickers.oldfiles({}, { window = { config = cursor_win_config } })
+			end
+
+			-- Modify the 'buffers' picker directly
 			MiniPick.registry.buffers = function()
 				return MiniPick.builtin.buffers({}, { window = { config = right_win_config } })
 			end
