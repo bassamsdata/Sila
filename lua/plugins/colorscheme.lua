83150df9b4e733fbad49f705aabf492f9a98e6a4
Bassam Data <105807570+bassamsdata@users.noreply.github.com>
2024-01-15 02:26:52 -0500

    modifying color schemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -55,25 +55,4 @@ return {
 			},
 		},
 	},
-	{
-		"Mofiqul/dracula.nvim",
-		lazy = true,
-		-- priority = 1000, -- make sure to load this before all the other start plugins
-		opts = {
-			colors = {
-				bg = "#07080f",
-				fg = "#C0B9DD",
-			},
-			transparent_bg = true,
-			italic_comment = true,
-			overrides = {
-				CursorLine = { bg = "#101c24" },
-				NormalFloat = { bg = nil }, --#transparency
-				["@conditional"] = { italic = true },
-				MiniMapSymbolView = { fg = "#323a54" },
-				UserStatusMode_NORMAL = { bg = "#6272a4" },
-				StatusLine = { guibg = NONE },
-			},
-		},
-	},
 }
