44ce811be9cef59026527f7a57b7c9280be78e52
Bassam Data <105807570+bassamsdata@users.noreply.github.com>
2024-01-15 02:24:15 -0500

    adding qf keymaps

---
+++ ftplugin/qf.lua
@@ -0,0 +1,13 @@
+vim.bo.buflisted = false
+vim.opt_local.spell = false
+vim.opt_local.rnu = false
+vim.opt_local.signcolumn = "no"
+vim.opt_local.statuscolumn = ""
+
+-- stylua: ignore start
+vim.keymap.set('n', '<Tab>', '<CR>zz<C-w>p',  { buffer = true })
+vim.keymap.set('n', '<C-j>', 'j<CR>zz<C-w>p', { buffer = true })
+vim.keymap.set('n', '<C-k>', 'k<CR>zz<C-w>p', { buffer = true })
+vim.keymap.set('n', '<C-n>', 'j<CR>zz<C-w>p', { buffer = true })
+vim.keymap.set('n', '<C-p>', 'k<CR>zz<C-w>p', { buffer = true })
+-- stylua: ignore end
