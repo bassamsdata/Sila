431ee91e54d92464a7bf145249282d0ad64bb049
Bassam Data <105807570+bassamsdata@users.noreply.github.com>
2024-01-27 17:01:40 -0500

    fix: fix the resize vim issues with Mini.Notify

--- lua/plugins/otherminis.lua
+++ lua/plugins/otherminis.lua
@@ -1,80 +1,11 @@
 return {
 	-- TODO: change the highlight color of the view map 'MiniMapSymbolView'
 	-- and add mini map to the help pages as well :)
-	-- {
-	-- 	"echasnovski/mini.notify",
-	-- 	event = "VeryLazy",
-	-- 	config = function()
-	-- 		local mini_notify = require("mini.notify")
-	-- 		vim.notify = mini_notify.make_notify()
-	-- 		-- print = mini_notify.make_notify()
-	-- 		vim.api.nvim_echo = mini_notify.make_notify()
-	-- 		-- TODO: make it work,the actual message content in different color and italic and add the level name like `ERROR`
-	-- 		-- ref; https://github.com/j-hui/fidget.nvim/blob/main/lua/fidget/notification/window.lua#L410
-	-- 		local cus_format = function(notif)
-	-- 			local time = vim.fn.strftime("%H:%M:%S", math.floor(notif.ts_update))
-	-- 			local icon = "\nNotification ❰❰" -- TODO: the problem with this that I need to make it complex to justify the icon ❰❰ to the right
-	-- 			return string.format("%s │ %s %s", time, notif.msg, icon)
-	-- 		end
-	-- 		local row = function()
-	-- 			local has_statusline = vim.o.laststatus > 0
-	-- 			local bottom_space = vim.o.cmdheight + (has_statusline and 1 or 0)
-	-- 			return vim.o.lines - bottom_space
-	-- 		end
-	-- 		mini_notify.setup({
-	-- 			content = {
-	-- 				format = nil,
-	-- 			},
-	-- 			lsp_progress = {
-	-- 				duration_last = 500,
-	-- 			},
-	-- 			window = {
-	-- 				config = {
-	-- 					-- all of that to do `Notification ❰❰` on the right side
-	-- 					border = "solid",
-	-- 					-- { -- different style
-	-- 					-- 	"",
-	-- 					-- 	"─",
-	-- 					-- 	"┐",
-	-- 					-- 	"│",
-	-- 					-- 	"",
-	-- 					-- 	"",
-	-- 					-- 	"",
-	-- 					-- 	"",
-	-- 					-- },
-	-- 					anchor = "SE",
-	-- 					row = row(),
-	-- 					title = "Notification ❰❰",
-	-- 					title_pos = "right",
-	-- 				},
-	-- 				winblend = 25,
-	-- 			},
-	-- 		})
-	-- 	end,
-	-- },
-	{
-		"echasnovski/mini.misc",
-		opts = {},
-		keys = {
-			{ "<leader>mm", "<cmd>lua MiniMisc.zoom()<cr>", desc = "Zoom" },
-			{
-				"<leader>ur",
-				"<cmd>lua MiniMisc.setup_auto_root()<cr>",
-				desc = "[U]I [Root]",
-			},
-		},
-	},
-	{ "echasnovski/mini.sessions", event = "VeryLazy", opts = {} },
-	{
-		"echasnovski/mini.align",
-		keys = {
-			{ "ga", mode = { "v", "n" } },
-			{ "gA", mode = { "v", "n" } },
-		},
-		opts = {},
-	},
 	{
 		"echasnovski/mini.map",
+		cond = function()
+			return not vim.b.large_file
+		end,
 		event = "FileType lua,norg,quarto",
 		config = function()
 			local map = require("mini.map")
@@ -112,42 +43,115 @@ return {
 			end
 		end,
 	},
+
 	{
+		"echasnovski/mini.notify",
+		event = "VeryLazy",
+		config = function()
+			local mini_notify = require("mini.notify")
+			vim.notify = mini_notify.make_notify()
+			-- FIX: convert the output to strings so it can appear in notify
+			-- print = mini_notify.make_notify()
+			-- vim.api.nvim_echo = mini_notify.make_notify()
+			local cus_format = function(notif)
+				local time = vim.fn.strftime("%H:%M:%S", math.floor(notif.ts_update))
+				local icon = "\nNotification ❰❰"
+				return string.format("%s │ %s %s", time, notif.msg, icon)
+			end
+			local row = function()
+				local has_statusline = vim.o.laststatus > 0
+				local bottom_space = vim.o.cmdheight + (has_statusline and 1 or 0)
+				return vim.o.lines - bottom_space
+			end
+
+			mini_notify.setup({
+				window = {
+					config = function()
+						return {
+							col = vim.o.columns - 2,
+							row = row(),
+							anchor = "SE",
+							title = "Notification ❰❰",
+							title_pos = "right",
+							---TODO: make max width 60%
+							-- width = 50,
+							border = "solid",
+						}
+					end,
+				},
+				-- content = {
+				-- 	format = nil,
+				-- },
+				-- lsp_progress = {
+				-- 	duration_last = 500,
+				-- },
+			})
+		end,
+	},
+
+	{
+		"echasnovski/mini.misc",
+		opts = {},
+		keys = {
+			{ "<leader>mm", "<cmd>lua MiniMisc.zoom()<cr>", desc = "Zoom" },
+			{
+				"<leader>ur",
+				"<cmd>lua MiniMisc.setup_auto_root()<cr>",
+				desc = "[U]I [Root]",
+			},
+		},
+	},
+
+	{ "echasnovski/mini.sessions", event = "VeryLazy", opts = {} },
+
+	{
+		"echasnovski/mini.align",
+		keys = {
+			{ "ga", mode = { "v", "n" } },
+			{ "gA", mode = { "v", "n" } },
+		},
+		opts = {},
+	},
+
+	{ --- TODO: incoprorate the apply_highlight function I made so It can
+		--- apply one highlight for bg and another for fg
 		"echasnovski/mini.hipatterns",
+		cond = function()
+			return not vim.b.large_file
+		end,
 		event = "BufReadPost",
 		config = function()
 			local mini_hipatterns = require("mini.hipatterns")
 
 			if mini_hipatterns then
-				mini_hipatterns.setup({
-					highlighters = {
-						hex_color = mini_hipatterns.gen_highlighter.hex_color(),
-						todo = {
-							-- TODO: test
-							pattern = "TODO",
-							group = "SkyBlue_bg",
-							extmark_opts = { sign_text = "󰸞", sign_hl_group = "SkyBlue" },
+				local keywords = { "NOTE", "BUG", "LOVE", "TODO", "TEST", "FIX" }
+				local icons = { "󱑴", "󱑬", "󱑭", "󱑯" }
+				local highlighters = {
+					hex_color = mini_hipatterns.gen_highlighter.hex_color(),
+				}
+
+				for _, keyword in ipairs(keywords) do
+					local lowerKeyword = string.lower(keyword)
+					local highlightGroup = string.format("HiPatterns%s", keyword)
+					highlighters[lowerKeyword] = {
+						pattern = string.format("%s:", keyword),
+						group = highlightGroup,
+						extmark_opts = {
+							sign_text = icons[_],
+							sign_hl_group = highlightGroup,
 						},
-						todo_trail = {
-							pattern = "TODO: ()%S+.*()",
-							group = "SkyBlue",
-						},
-						-- LOVE: do some love
-						love = {
-							pattern = "LOVE",
-							group = "Ochre_bg",
-							extmark_opts = { sign_text = "󰩖", sign_hl_group = "Ochre" },
-						},
-						-- highlight the text after LOVE with different highlightv
-						love_trail = {
-							pattern = "LOVE: ()%S+.*()",
-							group = "Ochre",
-						},
-					},
-				})
+					}
+					highlighters[string.format("%s_trail", lowerKeyword)] = {
+						pattern = string.format("%s: ()%%S+.*()", keyword),
+						group = highlightGroup,
+					}
+				end
+
+				mini_hipatterns.setup({ highlighters = highlighters })
 			end
 		end,
 	},
+
 	{
 		"echasnovski/mini.bufremove",
 		keys = {
