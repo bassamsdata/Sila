36fd70af9ca06eb2d04d2a1abe03ad300f5c0dc8
Bassam Data <105807570+bassamsdata@users.noreply.github.com>
2024-02-03 15:27:42 -0500

    feat: using a lua file for the colorswitch module

--- plugin/colorswitch.lua
+++ plugin/colorswitch.lua
@@ -1,12 +1,50 @@
-local utils = require("utils")
-local colors_file = string.format(vim.fn.stdpath("state") .. "colors.json")
--- 1. Restore dark/light background and colorscheme from json so that nvim
---    "remembers" the background and colorscheme when it is restarted.
--- 2. Spawn setbg/setcolors on colorscheme change to make other nvim instances
---    and system color consistent with the current nvim instance.
+-- Modifeied verstion using lua instead of json. credit to macro-nvim, https://github.com/Bekaboo/nvim/blob/master/plugin/colorswitch.lua
+-- TODO: why not trying to use a table inside this instead of a separate file
 
-local saved = utils.read(colors_file)
-saved.colors_name = saved.colors_name or "dragon"
+-- stylua: ignore start
+local colors_file =
+vim.fs.joinpath( vim.fn.stdpath("config") --[[@as string]], "lua/color_scheme.lua")
+-- stylua: ignore end
+
+local M = {}
+
+function M.handle_file(action, path, str)
+	local file = io.open(path, action == "read" and "r" or "w")
+	if not file then
+		return action == "read" and nil or false
+	end
+	if action == "read" then
+		local content = file:read("*a")
+		file:close()
+		return content or ""
+	else
+		file:write("return {\n")
+		for key, value in pairs(str) do
+			file:write(key .. ' = "' .. value .. '",\n')
+		end
+		file:write("}\n")
+		file:close()
+		return true
+	end
+end
+
+-- FIX: enhance this process since we are requiring the file twice - maybe pcall would help
+-- local file = io.open("color_scheme.lua", "r")
+-- if file == nil then
+-- 	file = io.open("color_scheme.lua", "w")
+-- 	file:write('return {colors_name = "nano", bg = vim.go.bg}\n')
+-- 	file:close()
+-- else
+-- 	file:close()
+-- end
+
+local saved = require("color_scheme")
+-- if not ok then
+-- 	local file = io.open("color_scheme.lua", "w")
+-- 	file:write('return {"colors_name = "nano", bg = vim.go.bg"}\n')
+-- 	file:close()
+-- end
+saved.colors_name = saved.colors_name or "nano"
 
 if saved.bg and saved.bg ~= vim.go.bg then
 	vim.go.bg = saved.bg
@@ -23,13 +61,15 @@ vim.api.nvim_create_autocmd("Colorscheme", {
 	group = vim.api.nvim_create_augroup("ColorSwitch", {}),
 	desc = "Spawn setbg/setcolors on colorscheme change.",
 	callback = function()
-		local data = utils.read(colors_file)
+		local data = require("color_scheme")
 		if data.colors_name ~= vim.g.colors_name or data.bg ~= vim.go.bg then
 			data.colors_name = vim.g.colors_name
 			data.bg = vim.go.bg
-			if not utils.write(colors_file, data) then
+			if not M.handle_file("write", colors_file, data) then
 				return
 			end
 		end
 	end,
 })
+
+return M
