d96a5d72e94c1d0980de6596c76c72388333232f
Bassam Data <105807570+bassamsdata@users.noreply.github.com>
2024-02-13 14:22:37 -0500

    feat: add undotree plugin

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -4,7 +4,7 @@ return {
 		dependencies = "nvim-lua/plenary.nvim",
 		config = true,
 		keys = {
-			{ "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
+			{ "<leader>ut", "<cmd>lua require('undotree').toggle()<cr>" },
 		},
 	},
 }
